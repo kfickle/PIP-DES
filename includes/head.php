90904a883f9de1289b63c51e5f4df60259814ab7
Kahela Fickle <38086287+kfickle@users.noreply.github.com>
2020-08-18 16:04:31 -0700

    php structure & body of template; end of day committed; 90% done

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 <head>
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
 	<title>
 		<?php echo $pagetitle; ?>
 	</title>
--- index.php
+++ index.php
@@ -2,38 +2,39 @@
 <html lang="en">
 
 <?php
-	
+
 	// set varible meta content for use in head.php include file
-	
-    $pagetitle = "page title variable?";
-    $metadescription = ""; 
+
+    $pagetitle = "";
+    $metadescription = "";
     $metakeywords = "";
-    $authorname = "PSU Student Media Tech"; 
+    $authorname = "PSU Student Media Tech";
 
 	// call include files
-	
+
     include_once './includes/head.php';
-	include_once './includes/navigation.php';
+		include_once './includes/navigation.php';
 	?>
 
 <body>
 
-	<!-- Masthead-->
+	<!-- page-specific generated content starts-->
 	<main class="page-content">
-		<div class="container h-100">
+		<div class="container-fluid h-100">
 			<div class="row h-100 align-items-center justify-content-center text-center">
 				<div class="col-lg-10 align-self-end">
-					<h1 class="text-uppercase">Organization Name</h1>
-					<hr class="divider my-4" />
+					<h1>Organization Name</h1>
 				</div>
-				<div class="col-lg-8 align-self-baseline">
-					<p class="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-					<a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+				<div class="col-xl-6 col-lg-8 col-md-11 col-sm-11 col-11 align-self-baseline">
+					<div class="mt-5 mb-4 card">
+						<p class="m-3 m-md-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin egestas laoreet. Ut varius risus vel eros feugiat convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam aliquet lacinia sapien at interdum. Pellentesque at mi in est dictum ultricies. Sed non ante justo. Sed efficitur vulputate ligula eget elementum. Integer rhoncus elementum neque vitae euismod.</p>
+					</div>
+					<a class="btn connect-btn" href="#">Connect</a>
 				</div>
 			</div>
 		</div>
 	</main>
-
+	<!-- page-specific generated content ends -->
 	<?php include_once './includes/footer.php'; ?>
 </body>
 
